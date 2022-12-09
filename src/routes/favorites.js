b728f81676be97d8aedf436b7f07b7e144a668f8
GonzaloRossin <gonzarossin@gmail.com>
2022-12-09 00:44:10 -0300

    feat: add post and delete documents from favorites functionality

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -10,14 +10,29 @@ router.get('/:user_id', (req, res) => {
     
 });
 
-router.post('/:user_id/:document_id', (req, res) => {
+router.post('/:user_id/:document_id', async (req, res) => {
+
+    const data = await db.collection(process.env.COLLECTION_USERS)
+                        .updateOne({_id: ObjectId(req.params.user_id)}, {$push: { favorites: req.params.document_id}});
     
+    if(data){
+        res.status(200).send({message: 'document added to favorites'});
+    }else{
+        res.status(500).send({message: 'error while adding document to favorites'});
+    }
     
 });
 
-router.delete('/:user_id/:document_id', (req, res) => {
+router.delete('/:user_id/:document_id', async (req, res) => {
     
+    const data = await db.collection(process.env.COLLECTION_USERS)
+                        .updateOne({_id: ObjectId(req.params.user_id)}, {$pull: { favorites: req.params.document_id}});
     
+    if(data){
+        res.status(200).send({message: 'document removed from favorites'});
+    }else{
+        res.status(500).send({message: 'error while removing document to favorites'});
+    }
 });
 
 module.exports = router;
