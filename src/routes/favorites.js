56ef5b05deafa52841d87ba841d31ce89532f0b1
gonzaloRossin <gonzarossin@gmail.com>
2022-12-18 01:33:38 -0300

    fix: delete user now deletes its documents and favorite references

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -6,14 +6,12 @@ const db = require('../db/db.util').getDb();
 
 router.get('/:user_id', async (req, res) => {
 
-    var favorite_array = [];
+    var favorite_array;
     await db.collection(process.env.COLLECTION_USERS)
     .findOne({_id: ObjectId(req.params.user_id)})
     .then((result, err) => {
         if(!err){
-            result.favorites.forEach((id,i) => {
-                favorite_array.push(new ObjectId(id));
-            })
+            favorite_array = result.favorites;
         }
     });   
     
@@ -27,7 +25,7 @@ router.get('/:user_id', async (req, res) => {
 router.post('/:user_id/:document_id', async (req, res) => {
 
     const data = await db.collection(process.env.COLLECTION_USERS)
-                        .updateOne({_id: ObjectId(req.params.user_id)}, {$push: { favorites: req.params.document_id}});
+                        .updateOne({_id: ObjectId(req.params.user_id)}, {$push: { favorites: new ObjectId(req.params.document_id)}});
     
     if(data){
         res.status(200).send({message: 'document added to favorites'});
